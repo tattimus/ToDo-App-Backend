9011f6325becb5538a2d6613a8a09448497d02d8
Tattimus <samu.tatti@luukku.com>
2020-12-07 00:42:25 +0200

    cleaning pointless variables

--- utils/config.js
+++ utils/config.js
@@ -2,13 +2,7 @@ require('dotenv').config()
 
 const { PORT } = process.env
 const { APPLICATION_STAGE } = process.env
-const { COSMOS_DB_URI } = process.env
-const { EMAIL_USER } = process.env
-const { EMAIL_PASSWORD } = process.env
-const { AZURE_STORAGE_ACCOUNT_NAME } = process.env
-const { AZURE_STORAGE_ACCOUNT_ACCESS_KEY } = process.env
 const { NODE_ENV } = process.env
-const { SENDGRID_APIKEY } = process.env
 
 let MONGODB_URI
 if (process.env.NODE_ENV === 'test') {
@@ -22,11 +16,5 @@ module.exports = {
   PORT,
   MONGODB_URI,
   APPLICATION_STAGE,
-  COSMOS_DB_URI,
-  EMAIL_USER,
-  EMAIL_PASSWORD,
-  AZURE_STORAGE_ACCOUNT_NAME,
-  AZURE_STORAGE_ACCOUNT_ACCESS_KEY,
   NODE_ENV,
-  SENDGRID_APIKEY,
 }
